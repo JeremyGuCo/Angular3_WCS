e15a330780780acad1d1a755b6f6c592d32c05f7
JeremyGueCo <jeremy.guerincoquereau@gmail.com>
2019-03-27 10:43:25 +0100

    Quete 3 OK

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { HighlightDirective } from './movie-list/highlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieListComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
